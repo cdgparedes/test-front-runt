f0162851966c27913f150c6537aee009a0eeb446
cdgparedes <cdgparedes88@gmail.com>
2021-06-15 12:56:38 -0500

    front app prueba runt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsignaturaComponent } from './asignatura/asignatura.component';
+import { AsignaturaService } from './asignatura/asignatura.service';
+
+import { RouterModule, Routes} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+ 
+
+
+const routes: Routes = [
+  
+  { path: '', redirectTo: '/asignaturas', pathMatch: 'full' },
+  { path: 'asignaturas', component: AsignaturaComponent },
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsignaturaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AsignaturaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
